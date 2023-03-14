2f4b5fb070607acbc363e12a0def82a841ba01e2
Anders Boyum <Acboyum@gmail.com>
2023-03-14 12:56:54 -0500

    wrote first couple functions

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,27 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+
+// init global variable/array basket, empty
+let basket = [];
+
+// create function addItem, which adds given string input to basket array, returns true
+function addItem (item) {
+    // add given item to basket array
+    basket.push(item);
+    return true;
+}
+addItem('chips');
+addItem('carrots');
+
+console.log("Testing addItem, should return true:", addItem('cheese'));
+console.log('What is in my basket?', basket);
+
+
+// create function listItems, which loops over basket array and lists each individual item in array
+function listItems() {
+    for (let i = 0; i < basket.length; i++) {
+    console.log(basket[i]);
+}
+}
+console.log(listItems());
