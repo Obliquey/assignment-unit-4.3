41b3135363e3e4e3b0bb0dbdeb658d43c3a92d99
Anders Boyum <Acboyum@gmail.com>
2023-03-15 12:31:55 -0500

    finishing up stretch goals

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -2,14 +2,18 @@ console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
 
-// init global variable/array basket, empty
+// init global variable/array basket set to empty
 let basket = [];
+// init global const maxItems set to 5
+const maxItems = 5;
 
 // create function addItem, which adds given string input to basket array, returns true
 function addItem (item) {
     // add given item to basket array
-    basket.push(item);
-    return true;
+    if (isFull() = false){
+        basket.push(item);
+        return true;
+    }
 }
 addItem('chips');
 addItem('carrots');
@@ -25,5 +29,35 @@ function listItems() {
         // print individual item in array
         console.log(item);
     }
+    return "Finshed";
 }
 console.log(listItems());
+
+// create function empty, which emptys/resets the basket array
+function empty () {
+    basket.length = 0;
+    return "Basket emptied";
+}
+console.log(empty());
+addItem('chips');
+addItem('cups');
+addItem('blankets');
+addItem('copper');
+console.log(basket);
+
+
+// creat function isFull, checks against maxItems if basket array is full
+function isFull() {
+    if (basket.length < maxItems) {
+        console.log("You can fit more.");
+        return false;
+    }
+    else {
+        console.log('Too full!')
+        return true;
+    }
+}
+
+
+console.log("Checking if basket is full:", isFull());
+
